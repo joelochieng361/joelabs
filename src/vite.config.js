179395114e22de0df407dfdcb58fcfd85ecf4e6e
joelochieng361 <joelochieng361@gmail.com>
2026-04-02 09:47:17 +0300

    update

---
+++ src/vite.config.js
@@ -0,0 +1,8 @@
+import { defineconfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineconfig({
+    plugins: [react()],
+    base: process.env.VITE_BASE_PATH || '/joelabs',
+})
